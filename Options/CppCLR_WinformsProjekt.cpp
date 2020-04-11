1ce4c1b0c251022b481dbb3c74c2ccc26f4c0eb1
VVV <vvvofka@yandex.ru>
2020-04-12 02:34:45 +0300

    Ok

--- Options/CppCLR_WinformsProjekt.cpp
+++ Options/CppCLR_WinformsProjekt.cpp
@@ -1,13 +1,10 @@
 #include "pch.h"
 using namespace System;
-
 //int main(array<System::String ^> ^args)
 //{
 //    return 0;
 //}
-
 #include "Form1.h"
-
 using namespace System::Windows::Forms;
 
 [STAThread]
